ee5f94b8f0cf51b3f77530e935e1d7a05fbf0edf
youpy <youpy@buycheapviagraonlinenow.com>
2013-12-31 14:40:47 +0900

    remove unused scripts

--- app.js
+++ app.js
@@ -1,95 +1,95 @@
-$(function() {
-  (function(global) {
-    var App = function() {
-      this.init();
-    };
+(function(global) {
+  var App = function() {
+    this.init();
+  };
+
+  _.extend(App.prototype, {
+    init: function() {
+      this.instruments = [];
+      this.tabs        = [];
+      this.timers      = [];
+      this.started     = false;
+      this.intervals   = [];
+    },
     
-    $.extend(App.prototype, {
-      init: function() {
-        this.instruments = [];
-        this.tabs        = [];
-        this.timers      = [];
-        this.started     = false;
-        this.intervals   = [];
-      },
-      
-      register: function(url, options) {
-        var options = options || {};
+    register: function(url, options) {
+      var options = options || {};
 
-        this.instruments.push([url, options]);
-      },
+          this.instruments.push([url, options]);
+    },
 
-      start: function() {
-        var that = this;
+    start: function() {
+      var that = this;
 
-        this.started = true;
+      this.started = true;
 
-        var intervalId = setInterval(function() {
-          var tab = that.tabs[Math.floor(Math.random() * that.tabs.length)];
+      var intervalId = setInterval(function() {
+        var tab = that.tabs[Math.floor(Math.random() * that.tabs.length)];
 
-          if(tab.active)
-            chrome.tabs.update(tab.id, { active: true });
-        }, 500);
+        if(tab.active)
+          chrome.tabs.update(tab.id, { active: true });
+      }, 500);
 
-        this.intervals.push(intervalId);
+      this.intervals.push(intervalId);
 
-        _.each(this.instruments, function(instrument) {
-          var url     = instrument[0],
-              options = instrument[1],
-              timer;
+      _.each(this.instruments, function(instrument) {
+        var url     = instrument[0],
+        options = instrument[1],
+        timer;
 
-          timer = setTimeout(function() {
-            chrome.tabs.create({
-              url: url
-            }, function(tab) {
-              var timer;
+        timer = setTimeout(function() {
+          chrome.tabs.create({
+            url: url
+          }, function(tab) {
+            var timer;
 
-              that.tabs.push(tab);
+            that.tabs.push(tab);
 
-              if(options.closeAfter) {
-                timer = setTimeout(function() {
-                  if(tab.active)
-                    chrome.tabs.remove(tab.id);
+            if(options.closeAfter) {
+              timer = setTimeout(function() {
+                if(tab.active)
+                  chrome.tabs.remove(tab.id);
 
-                  that.tabs.splice(that.tabs.indexOf(tab), 1);
+                that.tabs.splice(that.tabs.indexOf(tab), 1);
 
-                  if(options.onClose)
-                    options.onClose.apply(null, []);
-                }, options.closeAfter);
+                if(options.onClose)
+                  options.onClose.apply(null, []);
+              }, options.closeAfter);
 
-                that.timers.push(timer);
-              }
-            });
-          }, options.wait || 0);
+              that.timers.push(timer);
+            }
+          });
+        }, options.wait || 0);
 
-          that.timers.push(timer);
-        });
-      },
+        that.timers.push(timer);
+      });
+    },
 
-      stop: function() {
-        this.started = false;
+    stop: function() {
+      this.started = false;
 
-        _.each(this.tabs, function(tab) {
-          if(tab.active)
-            chrome.tabs.remove(tab.id);
-        });
+      _.each(this.tabs, function(tab) {
+        if(tab.active)
+          chrome.tabs.remove(tab.id);
+      });
 
-        _.each(this.timers, function(timer) {
-          clearTimeout(timer);
-        });
+      _.each(this.timers, function(timer) {
+        clearTimeout(timer);
+      });
 
-        _.each(this.intervals, function(interval) {
-          clearInterval(interval);
-        });
+      _.each(this.intervals, function(interval) {
+        clearInterval(interval);
+      });
 
-        this.tabs    = [];
-        this.timers  = [];
-        this.started = false;
-      }
-    });
+      this.tabs    = [];
+      this.timers  = [];
+      this.started = false;
+    }
+  });
 
-    global.Soundwwwalk = global.Soundwwwalk || {};
-    global.Soundwwwalk.App = App;
-  })(window);
-});
+  global.Soundwwwalk = global.Soundwwwalk || {};
+  global.Soundwwwalk.App = App;
+})(window);
+
+
 
--- background.js
+++ background.js
@@ -1,99 +1,96 @@
-$(function() {
-  var app = new Soundwwwalk.App();
+var app = new Soundwwwalk.App();
 
-  app.register('http://youtu.be/CsGtzbwXwSo?t=1m35s', {
-    closeAfter: (8 * 60 + 19) * 1000
-  });
-
-  app.register('http://www.youtube.com/watch?v=6_PLnInsh7E', {
-    closeAfter: (5 * 60 + 30) * 1000
-  });
-
-  app.register('http://www.youtube.com/watch?v=1iizIulzogs', {
-    wait: 60000,
-    closeAfter: (5 * 60) * 1000
-  });
-
-  app.register('http://youtu.be/3AhSUOiokBs?t=25s', {
-    wait: 300000,
-    closeAfter: 4 * 60 * 1000
-  });
-
-  // app.register('http://www.youtube.com/watch?v=MnPobgmdYXU', {
-  //   wait: 400000,
-  //   closeAfter: (4 * 60) * 1000
-  // });
-
-  app.register('http://www.youtube.com/watch?v=0eQjn8I7G4M', {
-    wait: 430000,
-    closeAfter: (2 * 60 + 20) * 1000
-  });
-
-  app.register('http://youtu.be/lE3N5N-Ueng?t=2m7s', {
-    wait: 400000,
-    closeAfter: (2 * 60) * 1000
-  });
-
-  app.register('http://www.youtube.com/watch?v=vROdVsU_K80', {
-    wait: 320000,
-    closeAfter: (1 * 60 + 40) * 1000
-  });
-
-  // app.register('http://youtu.be/PF7m-ztTRWY?t=24s', {
-  //   wait: 500000,
-  //   closeAfter: (8 * 60 + 30) * 1000
-  // });
-
-  app.register('http://youtu.be/dq6T5BojXc8?t=11s', {
-    wait: 500000,
-    closeAfter: 3 * 60 * 1000
-  });
-
-  app.register('http://youtu.be/PF7m-ztTRWY?t=24s', {
-    wait: 600000,
-    closeAfter: (8 * 60 + 30) * 1000
-  });
-
-  app.register('http://youtu.be/HS56AYVj0mo?t=9s', {
-    wait: 650000,
-    closeAfter: 2 * 60 * 1000
-  });
-
-  app.register('http://youtu.be/4pa5pSOkOus', {
-    wait: 660000,
-    closeAfter: 2 * 60 * 1000
-  });
-
-  app.register('http://www.youtube.com/watch?v=1F_IP5fHAvw', {
-    wait: 700000,
-    closeAfter: (60 * 7 + 10) * 1000,
-    onClose: function() {
-      app.stop();
-    }
-  });
-
-  app.register('http://youtu.be/web4QKwSzS8', {
-    wait: 750000,
-    closeAfter: 30 * 1000
-  });
-
-  app.register('http://youtu.be/ph2U28IidA4?t=24s', {
-    wait: 780000,
-    closeAfter: (2 * 60 + 20) * 1000
-  });
-
-  app.register('http://youtu.be/38Sh2BO6wZs?t=1m3s', {
-    wait: 830000,
-    closeAfter: (4 * 60 + 30) * 1000
-  });
-
-  setTimeout(function() {
-    if(app.started)
-      app.stop();
-  }, 20 * 60 * 1000);
-
-  chrome.browserAction.onClicked.addListener(function() {
-    app.started ? app.stop() : app.start();
-  });
+app.register('http://youtu.be/CsGtzbwXwSo?t=1m35s', {
+  closeAfter: (8 * 60 + 19) * 1000
 });
 
+app.register('http://www.youtube.com/watch?v=6_PLnInsh7E', {
+  closeAfter: (5 * 60 + 30) * 1000
+});
+
+app.register('http://www.youtube.com/watch?v=1iizIulzogs', {
+  wait: 60000,
+  closeAfter: (5 * 60) * 1000
+});
+
+app.register('http://youtu.be/3AhSUOiokBs?t=25s', {
+  wait: 300000,
+  closeAfter: 4 * 60 * 1000
+});
+
+// app.register('http://www.youtube.com/watch?v=MnPobgmdYXU', {
+//   wait: 400000,
+//   closeAfter: (4 * 60) * 1000
+// });
+
+app.register('http://www.youtube.com/watch?v=0eQjn8I7G4M', {
+  wait: 430000,
+  closeAfter: (2 * 60 + 20) * 1000
+});
+
+app.register('http://youtu.be/lE3N5N-Ueng?t=2m7s', {
+  wait: 400000,
+  closeAfter: (2 * 60) * 1000
+});
+
+app.register('http://www.youtube.com/watch?v=vROdVsU_K80', {
+  wait: 320000,
+  closeAfter: (1 * 60 + 40) * 1000
+});
+
+// app.register('http://youtu.be/PF7m-ztTRWY?t=24s', {
+//   wait: 500000,
+//   closeAfter: (8 * 60 + 30) * 1000
+// });
+
+app.register('http://youtu.be/dq6T5BojXc8?t=11s', {
+  wait: 500000,
+  closeAfter: 3 * 60 * 1000
+});
+
+app.register('http://youtu.be/PF7m-ztTRWY?t=24s', {
+  wait: 600000,
+  closeAfter: (8 * 60 + 30) * 1000
+});
+
+app.register('http://youtu.be/HS56AYVj0mo?t=9s', {
+  wait: 650000,
+  closeAfter: 2 * 60 * 1000
+});
+
+app.register('http://youtu.be/4pa5pSOkOus', {
+  wait: 660000,
+  closeAfter: 2 * 60 * 1000
+});
+
+app.register('http://www.youtube.com/watch?v=1F_IP5fHAvw', {
+  wait: 700000,
+  closeAfter: (60 * 7 + 10) * 1000,
+  onClose: function() {
+    app.stop();
+  }
+});
+
+app.register('http://youtu.be/web4QKwSzS8', {
+  wait: 750000,
+  closeAfter: 30 * 1000
+});
+
+app.register('http://youtu.be/ph2U28IidA4?t=24s', {
+  wait: 780000,
+  closeAfter: (2 * 60 + 20) * 1000
+});
+
+app.register('http://youtu.be/38Sh2BO6wZs?t=1m3s', {
+  wait: 830000,
+  closeAfter: (4 * 60 + 30) * 1000
+});
+
+setTimeout(function() {
+  if(app.started)
+    app.stop();
+}, 20 * 60 * 1000);
+
+chrome.browserAction.onClicked.addListener(function() {
+  app.started ? app.stop() : app.start();
+});
