4e8593574c56bfaa28f13f48106246e51c314e83
youpy <youpy@buycheapviagraonlinenow.com>
2013-12-31 14:37:37 +0900

    update

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ $(function() {
         this.tabs        = [];
         this.timers      = [];
         this.started     = false;
+        this.intervals   = [];
       },
       
       register : function(url, options) {
@@ -22,6 +23,15 @@ $(function() {
         var that = this;
 
         this.started = true;
+
+        var intervalId = setInterval(function() {
+          var tab = that.tabs[Math.floor(Math.random() * that.tabs.length)];
+
+          if(tab.active)
+            chrome.tabs.update(tab.id, { active: true });
+        }, 500);
+
+        this.intervals.push(intervalId);
 
         _.each(this.instruments, function(instrument) {
           var url     = instrument[0],
@@ -38,8 +48,13 @@ $(function() {
 
               if(options.closeAfter) {
                 timer = setTimeout(function() {
-                  if(tab.index >= 0)
+                  if(tab.active)
                     chrome.tabs.remove(tab.id);
+
+                  that.tabs.splice(that.tabs.indexOf(tab), 1);
+
+                  if(options.onClose)
+                    options.onClose.apply(null, []);
                 }, options.closeAfter);
 
                 that.timers.push(timer);
@@ -55,13 +70,16 @@ $(function() {
         this.started = false;
 
         _.each(this.tabs, function(tab) {
-          if(tab.index >= 0) {
+          if(tab.active)
             chrome.tabs.remove(tab.id);
-          }
         });
 
         _.each(this.timers, function(timer) {
           clearTimeout(timer);
+        });
+
+        _.each(this.intervals, function(interval) {
+          clearInterval(interval);
         });
 
         this.tabs    = [];
@@ -74,3 +92,4 @@ $(function() {
     global.Soundwwwalk.App = App;
   })(window);
 });
+
